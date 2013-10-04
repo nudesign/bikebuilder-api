53a42714eab5158d00312ae553fe962d4d3561c0
rodrigo brancher <rbrancher@gmail.com>
2013-10-04 17:06:11 -0300

    add build_types and component_types seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,120 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+road = BuildType.create({name: 'road'})
+fixie = BuildType.create({name: 'fixed gear'})
+city = BuildType.create({name: 'city'})
+
+road_component_types = [
+  'Frame',
+  'Fork',
+  'Front rim',
+  'Rear rim',
+  'Front spokes',
+  'Rear spokes',
+  'Front hub',
+  'Rear hub',
+  'Front tire',
+  'Front tire tube',
+  'Read tire',
+  'Rear tire tube',
+  'Cassette',
+  'Chain',
+  'Crankset',
+  'Pedals',
+  'Bottom bracket cartridge',
+  'Brake front',
+  'Brake rear',
+  'Brake lever Front',
+  'Brake lever Rear',
+  'Front shifter',
+  'Read shifter',
+  'Front derailleur',
+  'Rear derailleur',
+  'Stem',
+  'Bar',
+  'Seatpost',
+  'Saddle',
+  'Headset'
+]
+
+fixie_component_types = [
+  'Frame',
+  'Fork',
+  'Front rim',
+  'Rear rim',
+  'Front spokes',
+  'Rear spokes',
+  'Front hub',
+  'Rear hub',
+  'Front tire',
+  'Front tire tube',
+  'Read tire',
+  'Rear tire tube',
+  'Chain',
+  'Crankset',
+  'Pedals',
+  'Bottom bracket cartridge',
+  'Brake front',
+  'Brake rear',
+  'Brake lever Front',
+  'Brake lever Rear',
+  'Stem',
+  'Bar',
+  'Seatpost',
+  'Saddle',
+  'Headset'
+]
+
+city_component_types = [
+  'Frame',
+  'Fork',
+  'Front rim',
+  'Rear rim',
+  'Front spokes',
+  'Rear spokes',
+  'Front hub',
+  'Rear hub',
+  'Front tire',
+  'Front tire tube',
+  'Read tire',
+  'Rear tire tube',
+  'Cassette',
+  'Chain',
+  'Crankset',
+  'Pedals',
+  'Bottom bracket cartridge',
+  'Brake front',
+  'Brake rear',
+  'Brake lever Front',
+  'Brake lever Rear',
+  'Front shifter',
+  'Read shifter',
+  'Front derailleur',
+  'Rear derailleur',
+  'Stem',
+  'Bar',
+  'Seatpost',
+  'Saddle',
+  'Headset',
+  'Front Fender',
+  'Read Fender',
+  'Read Luggage rack',
+  'Front Luggage rack',
+  'Bell'
+]
+
+road_component_types.each do |name|
+  ct = ComponentType.find_or_create_by(name: name)
+  puts "ComponentType #{name} added to Road Bike." if road.components << ct
+end
+road.save
+
+fixie_component_types.each do |name|
+  ct = ComponentType.find_or_create_by(name: name)
+  puts "ComponentType #{name} added to Fixed Gear Bike." if fixie.components << ct
+end
+fixie.save
+
+city_component_types.each do |name|
+  ct = ComponentType.find_or_create_by(name: name)
+  puts "ComponentType #{name} added to City Bike." if city.components << ct
+end
+city.save
