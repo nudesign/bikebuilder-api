5d95e1b04541dbb57f87b581e50cd38da4360657
Rodrigo Pestana <rodrigo.pest@gmail.com>
2013-10-04 18:27:33 -0300

    add new build

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -4,4 +4,10 @@ class BuildsController < InheritedResources::Base
     @builds = Build.all
     render json: @builds
   end
+
+  def new
+    build_type = BuildType.where(name: params[:build_type_id]).first
+    @build = build_type.builds.new
+    render json: @build
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Bikebuilder::Application.routes.draw do
 
   resources :builds
+  get "builds/new/:build_type_id", to: "builds#new", as: "new_build_with_type"
+
   resources :components
 
 end
