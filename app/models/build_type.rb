f43fec0dd078855a09e0a91202e098988ed96b6b
rodrigo brancher <rbrancher@gmail.com>
2013-10-04 17:05:54 -0300

    fix relation declaration typos

--- app/models/build_type.rb
+++ app/models/build_type.rb
@@ -7,5 +7,5 @@ class BuildType
 
   # Relations
   has_many :builds, class_name: 'Build', inverse_of: 'type'
-  has_and_belongs_to_many :components, class_name: 'ComponentTypes', inverse_of: 'builds'
+  has_and_belongs_to_many :components, class_name: 'ComponentType', inverse_of: 'builds'
 end
--- app/models/component_type.rb
+++ app/models/component_type.rb
@@ -7,5 +7,5 @@ class ComponentType
 
   # Relations
   has_many :components, class_name: 'Component', inverse_of: 'type'
-  has_and_belongs_to_many :builds, class_name: 'BuildTypes', inverse_of: 'components'
+  has_and_belongs_to_many :builds, class_name: 'BuildType', inverse_of: 'components'
 end
