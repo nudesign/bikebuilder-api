ff27bef968e1ec8b411a185a1ec224c3cc2c3a77
rodrigo brancher <rbrancher@gmail.com>
2013-10-22 20:35:13 -0200

    outline builds#new controller specs

---
+++ spec/controllers/builds_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe BuildsController do
+
+  describe '#new' do
+
+    it 'instantiates a fresh build' do
+    end
+
+    it 'returns the fresh build as JSON' do
+    end
+
+    context 'requester specifies a build type' do
+
+      it 'sets the fresh build type as the requested type' do
+      end
+
+    end
+
+    context 'requester does not specify a build type' do
+
+      it 'sets the fresh build type as city bike' do
+      end
+
+    end
+
+  end
+
+
+  describe '#index' do
+
+    it 'assigns existing builds' do
+    end
+
+  end
+
+end
