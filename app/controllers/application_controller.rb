63cb0832d429eb1cd0bae0ed802c4912f6681f6d
rodrigo brancher <rbrancher@gmail.com>
2013-12-19 14:00:40 -0200

    set cors headers to accept calls from client-app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   respond_to :json
 
-  before_filter :set_default_response_format
+  before_filter :set_default_response_format, :set_cors_policy
 
   helper_method :current_user
 
   protected
     def set_default_response_format
       request.format = :json if params[:format].nil?
+    end
+
+    def set_cors_policy
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
 
   private
