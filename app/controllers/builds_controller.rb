0ad2b450872722a5e004f373640f35d1920b72b5
rodrigo brancher <rbrancher@gmail.com>
2013-12-19 14:01:02 -0200

    return build and build_type_components on builds#new

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -3,6 +3,7 @@ class BuildsController < InheritedResources::Base
   def new
     build_type = BuildType.where(name: params[:build_type_id]).first
     @build = build_type.builds.new
-    render json: @build
+
+    render json: {build: @build, build_type_components: build_type.components}
   end
 end
