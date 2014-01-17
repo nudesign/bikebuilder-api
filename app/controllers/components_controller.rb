2cfd5f3d2f41745c493d50705593089f6e3ced6e
rodrigo brancher <rbrancher@gmail.com>
2014-01-16 23:34:07 -0200

    creates components html crud

--- app/controllers/components_controller.rb
+++ app/controllers/components_controller.rb
@@ -1,2 +1,13 @@
 class ComponentsController < InheritedResources::Base
+
+  private
+    def build_resource_params
+      [params.fetch(:component, {}).permit([
+        :manufacturer_id,
+        :component_type_id,
+        :model,
+        :weight,
+        :cost
+      ])]
+    end
 end
