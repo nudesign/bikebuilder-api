8739fcd5a98ae95e00ffb8fb8903dbc5a253f038
rodrigo brancher <rbrancher@gmail.com>
2014-01-18 05:22:27 -0200

    creates component type crud

--- app/controllers/component_types_controller.rb
+++ app/controllers/component_types_controller.rb
@@ -1,3 +1,10 @@
 class ComponentTypesController < InheritedResources::Base
 
+  private
+    def build_resource_params
+      [params.fetch(:component_type, {}).permit([
+        :name,
+        :primary
+      ], build_type_ids: [])]
+    end
 end
